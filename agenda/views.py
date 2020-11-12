f040f83316650e1251dbf17553e3952c2b2006a9
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-11-12 16:34:27 -0300

    Update views.py

--- agenda/views.py
+++ agenda/views.py
@@ -92,9 +92,11 @@ def pegarURL(mesAgenda, anoAgenda=ano):
     corpo = soup.find('div', {'id': 'parent-fieldname-text'}) 
     elementos = corpo.find('ul')
     links =  elementos.findAll('a', href=True)
+    
     dicio = {}
 
     for a in links:
+        
         caminho = a['href'] 
         dia = a.text.strip() 
         dicio[dia] = f'{caminho}'
@@ -118,12 +120,14 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
         mesFormat = refMes
 
     calendario = pegarURL(mesNome(refMes), refAno) 
+    
     diaEventos = []
 
     for k, v in calendario.items(): 
         diaEventos.append(k)
 
     for k, v in calendario.items():
+        
         url = v
         cabecalho = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
         requisicao = requests.get(url, headers=cabecalho) 
@@ -135,7 +139,8 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
         elemento = corpo.findAll('tbody') 
         eventosConteudo = []
 
-        for i in elemento:         
+        for i in elemento:       
+            
             item = i.findAll('td')
             quantidade = len(item)
 
@@ -143,29 +148,37 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
                 pass
             
             else:
+                
                 for i in item:
+                    
                     itemTexto = (i.text).replace('\xa0','').replace('\n','').replace(';','').replace(',','')
                     eventosConteudo.append(itemTexto)
 
         conteudo = {} 
+        
         conteudo[diaEventos[0]] = eventosConteudo
         diaEventos.pop(0)
+        
         qtd = len(eventosConteudo)
+        
         resultado = []
 
         servico = ['1007', '1120', '1163', '1406', '1430', '1503', '1830', '1910', '1929', '1945', '5952', '5979', '5960', '5987', '2985', '2991',
                     '0561', '0588', '1889', '1708', '2852', '2879', '2950', '2976', '2003', '2011', '2020', '2100', '2119', '2208', '2216', '2658',
                     '2640', '2631', '3966', '8109', '8301', '3703', '6824', '6912', '0190', '0220', '9100', '9222', '7042', '7093', '9114',  '7122',
                     '0285', '0873', '4324', '4359', 'DAS (Documento de Arrecadação do Simples Nacional)', 'DAS-MEI (Documento de Arrecadação Simplificada do Microempreendedor Individual)']
+        
         comercio = ['9331', '8741', '1910', '5952', '5979', '5960', '5987', '2985', '2991', '0561', '0588', '1708', '8045', '2852', '2879', '2003',
                     '2011', '2100', '2119', '2208', '2607', '2615', '2704', '2712', '6106', '6505', '0924', '3699', '8109', '8301', '3703', '6824',
                     '6912', '1921', '0679', '0691', '0906', '2172', '6840', '5856', '1840', '0760', '0776', '0929', '0190', '2362', '5993', '3770',
                     '3746', '9100', '9222', '7042', '7093', '7114', '7122', '4983', '4990', '0285', '0873', '1136', '1165', '4020', '4042', '4324',
                     '4359', '4135', '4136', 'DAS (Documento de Arrecadação do Simples Nacional)', 'DAS-MEI (Documento de Arrecadação Simplificada do Microempreendedor Individual)',
                     '1077', '1120', '1163', '1406', '1473', '1503', '1830', '1910', '1929', '1945']
+        
         industria = ['1020', '3699', '9385', '3746', '6106', '6505', '5952', '5979', '5960', '5987', '2985', '2991', '0561', '0588', '3533', '3562',
                     '1708', '8045', '2119', '0676', '1097', '5110', '5123', '0668', '0821', '0838', '8109', '8301', '3703', '8496', '6912', '8645',
                     '5856', '2362', '5993', '3770', '3746', '9100', '9222', '7122', '1210', '3858', '4020', '4042', '2852', '2879', '2100']
+        
         financeiro = ['8053', '3426', '6800', '6813', '5273', '8468', '5557', '5232','5035', '5286', '0490', '1150', '4290', '5220', '6854', '7893',
                     '5952', '5979', '5960', '5987', '2985', '2991', '4574', '7987', '0561', '0588', '1889', '1708', '8045', '6106', '6505', '8109',
                     '8301', '6912', '2172', '5232', '0473', '0190', '4600', '8523', '6015', '2927', '0220', '5993', '3317', '0231', '0507', '2030',
@@ -177,6 +190,7 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
                 while qtd > 0:
                     
                     for k, v in conteudo.items():
+                        
                         d = f'{k}/{mesFormat}/{refAno}'
                         data = datetime.strptime(d, '%d/%m/%Y').date()
                         intervalo = timedelta(3)
@@ -184,22 +198,28 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
                         alerta = alerta.strftime('%d/%m/%Y')
 
                         if v[0] in servico:
+                            
                             resultado.append(f'{v[0]}, {k}/{mesFormat}/{refAno}, {k}/{mesFormat}/{refAno}, Verdadeiro, Verdadeiro, {alerta}, Competência: {v[2]}' + ' ' * 184 + f'https://receita.economia.gov.br/acesso-rapido/agenda-tributaria, {v[1]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
                         
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
             
             elif tipoAtv == 'C':
                 while qtd > 0:
                     
                     for k, v in conteudo.items():
+                        
                         d = f'{k}/{mesFormat}/{refAno}'
                         data = datetime.strptime(d, '%d/%m/%Y').date()
                         intervalo = timedelta(3)
@@ -207,22 +227,28 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
                         alerta = alerta.strftime('%d/%m/%Y')
 
                         if v[0] in comercio:
+                            
                             resultado.append(f'{v[0]}, {k}/{mesFormat}/{refAno}, {k}/{mesFormat}/{refAno}, Verdadeiro, Verdadeiro, {alerta}, Competência: {v[2]}' + ' ' * 184 + f'https://receita.economia.gov.br/acesso-rapido/agenda-tributaria, {v[1]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
                         
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
             
             elif tipoAtv == 'I':
                 while qtd > 0:
                     
                     for k, v in conteudo.items():
+                        
                         d = f'{k}/{mesFormat}/{refAno}'
                         data = datetime.strptime(d, '%d/%m/%Y').date()
                         intervalo = timedelta(3)
@@ -230,22 +256,28 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
                         alerta = alerta.strftime('%d/%m/%Y')
 
                         if v[0] in industria:
+                            
                             resultado.append(f'{v[0]}, {k}/{mesFormat}/{refAno}, {k}/{mesFormat}/{refAno}, Verdadeiro, Verdadeiro, {alerta}, Competência: {v[2]}' + ' ' * 184 + f'https://receita.economia.gov.br/acesso-rapido/agenda-tributaria, {v[1]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
                         
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
             
             elif tipoAtv == 'F':
                 while qtd > 0:
                     
                     for k, v in conteudo.items():
+                        
                         d = f'{k}/{mesFormat}/{refAno}'
                         data = datetime.strptime(d, '%d/%m/%Y').date()
                         intervalo = timedelta(3)
@@ -253,31 +285,40 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
                         alerta = alerta.strftime('%d/%m/%Y')
 
                         if v[0] in financeiro:
+                            
                             resultado.append(f'{v[0]}, {k}/{mesFormat}/{refAno}, {k}/{mesFormat}/{refAno}, Verdadeiro, Verdadeiro, {alerta}, Competência: {v[2]}' + ' ' * 184 + f'https://receita.economia.gov.br/acesso-rapido/agenda-tributaria, {v[1]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
                         
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
         
             else:
                 while qtd > 0:
                     
                     for k, v in conteudo.items():
+                        
                         d = f'{k}/{mesFormat}/{refAno}'
                         data = datetime.strptime(d, '%d/%m/%Y').date()
                         intervalo = timedelta(3)
                         alerta = (data - intervalo)
                         alerta = alerta.strftime('%d/%m/%Y')
+                        
                         resultado.append(f'{v[0]}, {k}/{mesFormat}/{refAno}, {k}/{mesFormat}/{refAno}, Verdadeiro, Verdadeiro, {alerta}, Competência: {v[2]}' + ' ' * 184 + f'https://receita.economia.gov.br/acesso-rapido/agenda-tributaria, {v[1]}\n')
+                        
                         del conteudo[k][0]
                         del conteudo[k][0]
                         del conteudo[k][0]
+                        
                         qtd = (qtd - 3)
 
         else:
@@ -286,78 +327,105 @@ def agenda(refMes, tipoDoc, tipoAtv, refAno=ano):
                 while qtd > 0:
 
                     for k, v in conteudo.items():
+                        
                         if v[0] in servico:
+                            
                             resultado.append(f'{k}/{mesFormat}/{refAno}; {v[0]}; {v[1]}; {v[2]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
 
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
 
             if tipoAtv == 'C':
                 while qtd > 0:
 
                     for k, v in conteudo.items():
+                        
                         if v[0] in comercio:
+                            
                             resultado.append(f'{k}/{mesFormat}/{refAno}; {v[0]}; {v[1]}; {v[2]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
 
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
 
             if tipoAtv == 'I':
                 while qtd > 0:
 
                     for k, v in conteudo.items():
+                        
                         if v[0] in industria:
+                            
                             resultado.append(f'{k}/{mesFormat}/{refAno}; {v[0]}; {v[1]}; {v[2]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
 
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
 
             if tipoAtv == 'F':
                 while qtd > 0:
 
                     for k, v in conteudo.items():
+                        
                         if v[0] in financeiro:
+                            
                             resultado.append(f'{k}/{mesFormat}/{refAno}; {v[0]}; {v[1]}; {v[2]}\n')
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
 
                         else:
+                            
                             del conteudo[k][0]
                             del conteudo[k][0]
                             del conteudo[k][0]
+                            
                             qtd = (qtd - 3)
                 
             else:
                 while qtd > 0:
 
                     for k, v in conteudo.items():
+                        
                         resultado.append(f'{k}/{mesFormat}/{refAno}; {v[0]}; {v[1]}; {v[2]}\n')
+                        
                         del conteudo[k][0]
                         del conteudo[k][0]
                         del conteudo[k][0]
+                        
                         qtd = (qtd - 3)
 
 
@@ -372,13 +440,16 @@ def saida(mesRef, tipoRef, tipoEmp):
     """
     
     requisicao = agenda(mesRef, tipoRef, tipoEmp)
+    
     agendaLista = []
+    
     agendaDicio = {}
 
     for i in requisicao:
         agendaLista.append(i)
     
     if tipoRef == 'virgula':
+        
         cabecalho = ['Assunto, Data de inicio, Data de termino, O dia inteiro, Lembrete ativado, Data do lembrete, Descricao, Local\n']
         agendaLista.insert(0, cabecalho)
         
@@ -399,21 +470,27 @@ def inicio(request):
     """
     
     if request.method == 'POST':
+        
         mesRef = int(request.POST.get(['mes-referencia'][0]))
         tipoRef = request.POST.get('tipo')
         tipoEmp = request.POST.get(['tipo-empresa'][0])
 
         try:
+            
             calendario = saida(mesRef, tipoRef, tipoEmp)
             calendario['tipo'] = tipoRef
 
         except:
+            
             msgErro = 'Mês de referência inválido, ou conteúdo indisponível!!'
             tab = ' '
+            
             calendario = {}
+            
             calendario['agenda'] = (f'\n\n\n{tab * 10}{msgErro}\
 \n\n{tab * 25}{msgErro}\
 \n\n{tab * 35}{msgErro}')
+            
             calendario['erro'] = 404
 
     else:
@@ -450,6 +527,7 @@ def contato(request):
     """
 
     if request.method == 'POST':
+        
         assunto = 'Agenda Tributária'
         nome = request.POST.get('nome-contato')
         email = request.POST.get('email-contato')
@@ -462,9 +540,11 @@ def contato(request):
 
         enviar = settings.EMAIL_HOST_USER
         destinatarios = ["de8ac25948-1f320b@inbox.mailtrap.io"]
+        
         send_mail(assunto, corpo, enviar, destinatarios)
 
         contato = {}
+        
         contato['sucesso'] = 1020
 
     else:
